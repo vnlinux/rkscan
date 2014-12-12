f1948158a2b164fdcf377489beef51db3357dd7f
kien kennedy <kiennd@gmail.com>
2014-12-12 21:37:32 +0700

    uniq the duplicate result

--- rkscan.sh
+++ rkscan.sh
@@ -4,13 +4,13 @@
 whitelist="bin|sbin|lib"
 
 # find the executed command not in whitelist
-rootkit=$(find /proc -type l -name "*exe" -exec ls -al {} + 2>/dev/null | awk '{if($11!="") print $11}' | grep -vE $whitelist)
+rootkit=$(find /proc -type l -name "*exe" -exec ls -al {} + 2>/dev/null | awk '{if($11!="") print $11}' | grep -vE $whitelist | sort -u)
 
 # result
 if [ -z "${rootkit}" ]; then
 	echo "Your system is clean."
 else
-	echo "List of rootkit $rootkit"
+	echo "Your system is infected with:\n$rootkit"
 fi
 
 exit
